21e5c7bdc94723954257dc232f2be8db176a7d83
Edno Silva Oliveira <ednosilvaoliveira269@gmail.com>
2025-03-04 21:19:32 -0300

    Sua mensagem de commit

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 
-
 const express = require('express');
 const app = express();
 
@@ -11,3 +10,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+
